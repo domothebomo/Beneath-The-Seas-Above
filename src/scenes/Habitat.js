d369496e4d20069da16a4b239784fe2b85cc0ca7
domothebomo <dfanaris@gmail.com>
2023-06-02 12:45:16 -0700

    removed old tutorial page

--- src/scenes/Habitat.js
+++ src/scenes/Habitat.js
@@ -85,38 +85,38 @@ class Habitat extends Phaser.Scene {
 
         keyH = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.H);
 
-        this.tutorialPanel = this.add.sprite(game.config.width/2,game.config.height/2, 'tutorial_panel').setOrigin(0.5,0.5).setDepth(101);
-        this.tutorialText = this.add.text(game.config.width/2 - 20,game.config.height/2 - 5,
-        `
-        As a newly promoted Senior Researcher of Extraterrestial Marine Biology, your first assignment
-        is to manage a research study beneath the unexplored waters of World-357.
+        // this.tutorialPanel = this.add.sprite(game.config.width/2,game.config.height/2, 'tutorial_panel').setOrigin(0.5,0.5).setDepth(101);
+        // this.tutorialText = this.add.text(game.config.width/2 - 20,game.config.height/2 - 5,
+        // `
+        // As a newly promoted Senior Researcher of Extraterrestial Marine Biology, your first assignment
+        // is to manage a research study beneath the unexplored waters of World-357.
 
-        When you open your visual interface, you will notice two tabs on the top and right side of your
-        screen. These will open two menus, indicated as "AREAS OF STUDY" and "LIFEFORMS". Areas of study
-        are where you will fuel your research using the biomass you collect from lifeforms. These
-        benchmarks progress from left to right, and will also unlock new lifeforms for you to introduce
-        to the environment. We have supplied you a small amount of biomass to kickstart your research, so
-        you can purchase the first benchmark and unlock your first lifeform right away.
+        // When you open your visual interface, you will notice two tabs on the top and right side of your
+        // screen. These will open two menus, indicated as "AREAS OF STUDY" and "LIFEFORMS". Areas of study
+        // are where you will fuel your research using the biomass you collect from lifeforms. These
+        // benchmarks progress from left to right, and will also unlock new lifeforms for you to introduce
+        // to the environment. We have supplied you a small amount of biomass to kickstart your research, so
+        // you can purchase the first benchmark and unlock your first lifeform right away.
 
-        The lifeforms menu is where you will artificially manufacture and place lifeforms into the
-        environment. If you have unlocked the lifeform through a benchmark and have enough biomass to
-        purchase one, left-clicking the lifeform's image will display a selection grid, which will
-        determine the location to place. Use left-click again to confirm the purchase and place the 
-        lifeform, or right-click to cancel. Lifeforms will naturally gather biomass over time, which can 
-        be extracted by clicking the syringe above them.
+        // The lifeforms menu is where you will artificially manufacture and place lifeforms into the
+        // environment. If you have unlocked the lifeform through a benchmark and have enough biomass to
+        // purchase one, left-clicking the lifeform's image will display a selection grid, which will
+        // determine the location to place. Use left-click again to confirm the purchase and place the 
+        // lifeform, or right-click to cancel. Lifeforms will naturally gather biomass over time, which can 
+        // be extracted by clicking the syringe above them.
 
-        Progress through the benchmarks and submit a report to complete your research. As an artesan of
-        science and explorer of the seas above, the Coalition is counting on you!
+        // Progress through the benchmarks and submit a report to complete your research. As an artesan of
+        // science and explorer of the seas above, the Coalition is counting on you!
 
-        Press H to open/close this tutorial
-        `, {
-            fontFamily: 'Verdana',
-            fontSize: '11px',
-            padding: {
-                top: 0,
-                bottom: 0,
-            }
-        }).setOrigin(0.5,0.5).setDepth(101);
+        // Press H to open/close this tutorial
+        // `, {
+        //     fontFamily: 'Verdana',
+        //     fontSize: '11px',
+        //     padding: {
+        //         top: 0,
+        //         bottom: 0,
+        //     }
+        // }).setOrigin(0.5,0.5).setDepth(101);
 
         this.tutorialTip = this.add.bitmapText(10, 380, 'unscreen_mk', 'Press H to Open Tutorial', 12);
         this.tutorialTip.alpha = 0;
@@ -315,4 +315,20 @@ class Habitat extends Phaser.Scene {
             }
         });
     }
+
+    rolloutDialogue(dialogue) {
+        let lines = this.dialogueText.getWrappedText(dialogue);
+        let text = lines.join('\n');
+
+        let letterCount = 0;
+        this.time.addEvent({
+            callback: () => {
+                this.dialogueText.text += text[letterCount];
+                letterCount += 1;
+            },
+            repeat: text.length - 1,
+            delay: 40
+        });
+    }
 }
+
